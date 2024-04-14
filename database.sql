f4180645050c167a5c8b2be72b687a6bb4d18e18
pavelkoA <pavelko88@mail.ru>
2024-04-14 22:22:44 +0300

    add db

--- database.sql
+++ database.sql
@@ -1,21 +1,21 @@
-drop table if exists sites cascade;
+drop table if exists urls cascade;
 drop table if exists url_check cascade;
 
-CREATE TABLE sites (
+CREATE TABLE urls (
 	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name varchar(255) unique not null,
-	created_at timestamp not null
+	created_at date default current_date
 );
 
 
 create table url_check (
 	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	site_id bigint REFERENCES sites (id)
+	site_id bigint REFERENCES urls (id)
 		on delete cascade
 		on update set null,
 	code_answer integer,
 	h1 varchar(255),
 	title varchar(255),
 	description text,
-	created_at timestamp not null
+	created_at date default current_date
 );
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 from page_analyzer.validator import get_url, is_url
 
@@ -9,6 +9,30 @@ from page_analyzer.validator import get_url, is_url
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
+
+
+def get_data_in_base_urls(connect, data):
+    with connect.cursor() as cursor:
+        cursor.execute(
+            f"""SELECT name
+                FROM urls
+                WHERE name = '{data}'"""
+        )
+        return cursor.fetchone()
+
+
+def write_data_to_base_urls(connect, data):
+    with connect.cursor() as cursor:
+        base_data = get_data_in_base_urls(connect, data)
+        if not base_data:
+            cursor.execute(
+                "INSERT INTO urls (name) VALUES (%s)",
+                [data]
+            )
+        connect.commit()
+        return get_data_in_base_urls(connect, data)
+
+
 
 
 app = Flask(__name__)
@@ -22,6 +46,15 @@ def get_index():
 @app.post("/urls")
 def create_url():
     site = request.form.get("url")
-    if is_url(site): 
-        return f'<h1>{get_url(site)}</h1>'
-    return '<h1>AAAAAAAAAAAAA</h1>'
+    
+    if is_url(site):
+        print(write_data_to_base_urls(conn, get_url(site)))
+        return f'<h1>URAAAAA!!!!</h1>'
+    return render_template(
+        "index.html"
+    )
+
+
+# @app.route("/urls/<id>")
+# def get_ulr_page(id):
+#     return 
