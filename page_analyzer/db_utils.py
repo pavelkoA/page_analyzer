0fb2bd94b1aabdc4b689abdd1350a94a1696c300
pavelkoA <pavelko88@mail.ru>
2024-04-23 12:23:15 +0300

    fix tests

--- page_analyzer/db_utils.py
+++ page_analyzer/db_utils.py
@@ -5,63 +5,63 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def read_url_by_name(url):
+    query = """SELECT *
+               FROM urls
+               WHERE name = %s"""
     with psycopg2.connect(DATABASE_URL) as connect:
         with connect.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(
-                """SELECT *
-                   FROM urls
-                   WHERE name = %s""", [url]
-            )
+            cursor.execute(query, [url])
             data = cursor.fetchone()
     connect.close()
     return data
 
 
 def read_url_by_id(id):
+    query = """SELECT *
+               FROM urls
+               WHERE id = %s"""
     with psycopg2.connect(DATABASE_URL) as connect:
         with connect.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(
-                """SELECT *
-                   FROM urls
-                   WHERE id = %s""", [id]
-            )
+            cursor.execute(query, [id])
             data = cursor.fetchone()
     connect.close()
     return data
 
 
 def read_checks(id):
+    query = """SELECT *
+               FROM url_checks
+               WHERE url_id = %s
+               ORDER BY id DESC"""
     with psycopg2.connect(DATABASE_URL) as connect:
         with connect.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(
-                """SELECT *
-                   FROM url_checks
-                   WHERE url_id = %s
-                   ORDER BY id DESC""", [id])
+            cursor.execute(query, [id])
             data = cursor.fetchall()
     connect.close()
     return data
 
 
 def read_urls_and_last_checks():
+    query = """SELECT
+                urls.id AS id,
+                urls.name AS name,
+                url_checks.status_code AS status_code,
+                url_checks.created_at AS created_at
+               FROM urls
+               LEFT JOIN url_checks
+               ON urls.id = url_checks.url_id
+               AND url_checks.id = (SELECT max(id)
+                FROM url_checks
+                WHERE urls.id = url_checks.url_id)
+               ORDER BY urls.id DESC;"""
     with psycopg2.connect(DATABASE_URL) as connect:
         with connect.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute("""SELECT
-                                urls.id AS id,
-                                urls.name AS name,
-                                url_checks.status_code AS status_code,
-                                url_checks.created_at AS created_at
-                              FROM urls
-                              LEFT JOIN url_checks
-                              ON urls.id = url_checks.url_id
-                              AND url_checks.id = (SELECT max(id)
-                                    FROM url_checks
-                                    WHERE urls.id = url_checks.url_id)
-                              ORDER BY urls.id DESC;""")
+            cursor.execute(query)
             data = cursor.fetchall()
     connect.close()
     return data
@@ -72,13 +72,16 @@ def write_url_checks(url_id,
                      h1,
                      title,
                      description):
+    query = """INSERT INTO url_checks
+               (url_id, status_code, h1, title, description)
+               VALUES (%s, %s, %s, %s, %s)"""
     with psycopg2.connect(DATABASE_URL) as connect:
         with connect.cursor() as cursor:
-            cursor.execute(
-                """INSERT INTO url_checks
-                   (url_id, status_code, h1, title, description)
-                   VALUES (%s, %s, %s, %s, %s)""",
-                [url_id, status_code, h1, title, description])
+            cursor.execute(query, [url_id,
+                                   status_code,
+                                   h1,
+                                   title,
+                                   description])
     connect.close()
 
 
